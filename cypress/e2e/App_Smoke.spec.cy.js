5c3b069113045b06880e210b5473137fbb9eb42a
Roshan Nair <roshannair89@gmail.com>
2023-03-27 15:39:50 -0600

    fifth commit

--- cypress/e2e/App_Smoke.spec.cy.js
+++ cypress/e2e/App_Smoke.spec.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://yaraworks.com')
+import { smokeTest } from './Smoke'
+
+Cypress._.each(['iphone-8','ipad-mini','samsung-s10'], viewport => {
+  it(`works on ${viewport}`, () => {
+    cy.Prdlogin()
+    cy.viewport(viewport)
+    smokeTest()
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,20 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('Prdlogin', () => {
+
+        
+    cy. clearCookies() 
+    cy. clearLocalStorage()
+    cy.setCookie('panda-user-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI4MjAzYzdlMS1mOWNhLTRhZjEtYTdkMC1kMGMwZDZjYWIxMDYiLCJhY2NvdW50UmVmZXJlbmNlIjoidGVsdXMiLCJ1c2VyVXVpZCI6IjI0ZmFiNGYyLTA0OTgtNDI4ZC04OTVjLTczZjUwYmNiOWNiYyIsImFjY291bnRVdWlkIjoiNzA3NjE3NDItMWJjZS00M2NlLWIwMjMtYjk5YmEwYTg0ZjY2IiwidXNlckVtYWlsIjoicm9zaGFuLm5haXJAdGVsdXMuY29tIiwic2NvcGVzIjpbInlhcmE6YWRtaW4iXSwiaWF0IjoxNjc5NzU3NzUwLCJleHAiOjE2ODAzNTc3NTB9.MwMDv2NviXqwm2h_STN-kRhkdg5Qzaxu_hOTTN5HAFo')
+    cy.setCookie('yara-user-calendar', 'GoogleCloud')
+    cy.setCookie('yara-account-slug','TELUS')
+    cy.setCookie('__Host-next-auth.csrf-token', '26d5bac015851a5ab953e4ac7757e7ac4f18c90d7b8ba4d9491d2e2fd2c398e1%7Ce87badaa8617a58313e7ec60fa05ba9c7fd24225891938f58fc56e22d3173951', {
+        secure: true
+    })
+    cy.setCookie('panda-account-uuid', '70761742-1bce-43ce-b023-b99ba0a84f66')
+    cy.setCookie('panda-user-auth-apps', '%5B%22admin%22%2C%22app%22%5D')
+    
+    
+    })
