590c8025c693b45edf3d6abeeedb195368a8e57c
Roshan Nair <roshannair89@gmail.com>
2023-03-27 23:24:45 -0600

    Test updated v4

--- cypress/e2e/App_Smoke.spec.cy.js
+++ cypress/e2e/App_Smoke.spec.cy.js
@@ -1,6 +1,16 @@
 import { smokeTest } from './Smoke'
+import { smokeTest } from './Smoke2'
 
 Cypress._.each(['iphone-8','ipad-mini','samsung-s10'], viewport => {
+  it(`works on ${viewport}`, () => {
+    cy.Prdlogin()
+    cy.viewport(viewport)
+    smokeTest()
+  })
+})
+
+
+Cypress._.each(['macbook-16'], viewport => {
   it(`works on ${viewport}`, () => {
     cy.Prdlogin()
     cy.viewport(viewport)
